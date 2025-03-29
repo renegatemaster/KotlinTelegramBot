b91b221222ceea750230f47da452970ec8a3b78b
renegatemaster <gleb.ostanin.dev@gmail.com>
2025-03-29 16:34:42 +0500

    KTB-04 parse words.txt and print

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,28 @@ package com.renegatemaster
 
 import java.io.File
 
+data class Word(
+    val original: String,
+    val translate: String,
+    val correctAnswersCount: Int = 0,
+)
+
 fun main() {
     val words = File("words.txt")
-    words.readLines().forEach { println(it) }
+    val dictionary = mutableListOf<Word>()
+
+    words.readLines().forEach {
+        val parts = it.split("|")
+        val original: String = parts.getOrNull(0) ?: ""
+        val translate: String = parts.getOrNull(1) ?: ""
+        val correctAnswersCount: Int? = parts.getOrNull(2)?.toIntOrNull()
+        dictionary.add(
+            Word(
+                original = original,
+                translate = translate,
+                correctAnswersCount = correctAnswersCount ?: 0,
+            )
+        )
+    }
+    println(dictionary)
 }
