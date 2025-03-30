a550212f9842dfb775323fe1825eb8de26d0dd65
renegatemaster <gleb.ostanin.dev@gmail.com>
2025-03-30 16:16:01 +0500

    KTB-05 impl trainer menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,15 @@ package com.renegatemaster
 
 import java.io.File
 
+const val MENU = "Меню:\n1 – Учить слова\n2 – Статистика\n0 – Выход"
+
 data class Word(
     val original: String,
     val translate: String,
     val correctAnswersCount: Int = 0,
 )
 
-fun main() {
+fun loadDictionary(): MutableList<Word> {
     val words = File("words.txt")
     val dictionary = mutableListOf<Word>()
 
@@ -16,14 +18,30 @@ fun main() {
         val parts = it.split("|")
         val original: String = parts.getOrNull(0) ?: ""
         val translate: String = parts.getOrNull(1) ?: ""
-        val correctAnswersCount: Int? = parts.getOrNull(2)?.toIntOrNull()
+        val correctAnswersCount: Int = parts.getOrNull(2)?.toIntOrNull() ?: 0
         dictionary.add(
             Word(
                 original = original,
                 translate = translate,
-                correctAnswersCount = correctAnswersCount ?: 0,
+                correctAnswersCount = correctAnswersCount,
             )
         )
     }
-    println(dictionary)
+
+    return dictionary
+}
+
+fun main() {
+    val dictionary = loadDictionary()
+
+    while (true) {
+        println(MENU)
+        val input = readln()
+        when (input) {
+            "1" -> println("Учить слова")
+            "2" -> println("Статистика")
+            "0" -> break
+            else -> println("Введите число 1, 2 или 0")
+        }
+    }
 }
