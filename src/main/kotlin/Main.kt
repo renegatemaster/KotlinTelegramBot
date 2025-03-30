6fae1351d4cf2316ae38523c1331217ccc8a5753
renegatemaster <gleb.ostanin.dev@gmail.com>
2025-03-30 16:47:27 +0500

    KTB-06 impl statistics

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,17 @@ package com.renegatemaster
 
 import java.io.File
 
+const val MENU = "Меню:\n1 – Учить слова\n2 – Статистика\n0 – Выход"
+const val LEARNED_COUNT = 3
+const val ONE_HUNDRED_PERCENT = 100
+
 data class Word(
     val original: String,
     val translate: String,
     val correctAnswersCount: Int = 0,
 )
 
-fun main() {
+fun loadDictionary(): MutableList<Word> {
     val words = File("words.txt")
     val dictionary = mutableListOf<Word>()
 
@@ -16,14 +20,36 @@ fun main() {
         val parts = it.split("|")
         val original: String = parts.getOrNull(0) ?: ""
         val translate: String = parts.getOrNull(1) ?: ""
-        val correctAnswersCount: Int? = parts.getOrNull(2)?.toIntOrNull()
+        val correctAnswersCount: Int = parts.getOrNull(2)?.toIntOrNull() ?: 0
         dictionary.add(
             Word(
                 original = original,
                 translate = translate,
-                correctAnswersCount = correctAnswersCount ?: 0,
+                correctAnswersCount = correctAnswersCount,
             )
         )
     }
-    println(dictionary)
+
+    return dictionary
+}
+
+fun main() {
+    val dictionary = loadDictionary()
+
+    while (true) {
+        println(MENU)
+        val input = readln()
+        when (input) {
+            "1" -> println("Учить слова")
+            "2" -> {
+                val totalCount = dictionary.size
+                val correctAnswersCount = dictionary.filter { it.correctAnswersCount >= LEARNED_COUNT }.size
+                val percent = correctAnswersCount * ONE_HUNDRED_PERCENT / totalCount
+                println("Выучено $correctAnswersCount из $totalCount слов | $percent%\n")
+            }
+
+            "0" -> break
+            else -> println("Введите число 1, 2 или 0")
+        }
+    }
 }
